003102d8d6b8911611f0dbe09c2e57879d3ecfc4
JohanCasserfelt <johan.casserfelt@gmail.com>
2019-11-13 16:24:24 +0100

    gameLoop for client side

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,55 +1,72 @@
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import questions.Questions;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Client extends JFrame {
 
 //172.20.202.126
 
-    JLabel label=new JLabel("Vad heter du?");
+    JLabel label = new JLabel("Vad heter du?");
     JButton[] buttons;
-    String[] alternativ={"Fazli","Johan","VictorO","VictorJ"};
-    JPanel panel=new JPanel();
-    Socket socket=new Socket("localhost",56565);
-    PrintWriter pw=new PrintWriter(socket.getOutputStream(),true);
+    String[] alternativ = {"Fazli", "Johan", "VictorO", "VictorJ"};
+    JPanel panel = new JPanel();
+    Socket socket = new Socket("172.20.202.139", 56565);
+    PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+    ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
 
     public Client() throws IOException {
 
 
-        buttons=new JButton[4];
+        buttons = new JButton[4];
         add(label, BorderLayout.NORTH);
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i]=new JButton(alternativ[i]);
+            buttons[i] = new JButton(alternativ[i]);
             buttons[i].addActionListener(clientListener);
             panel.add(buttons[i], BorderLayout.SOUTH);
         }
         add(panel);
-        setSize(400,400);
+        setSize(400, 400);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
     }
 
+    public void gameLoop() {
+        Object obj;
 
-    ActionListener clientListener=e->{
-        JButton temp=(JButton) e.getSource();
+        try {
+            while ((obj = in.readObject()) != null) {
+                if (obj instanceof Questions) {
+                    Questions q = (Questions) obj;
+                    //label.setText(q.getQuestion);
+
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+// throws IOException, ClassNotFoundException
+    }
+
+    ActionListener clientListener = e -> {
+        JButton temp = (JButton) e.getSource();
         pw.println(temp.getText());
     };
 
-
     public static void main(String[] args) {
         try {
-            new Client();
+            Client client = new Client();
         } catch (IOException e) {
             e.printStackTrace();
         }
