736cde2128475f58d3f5b3a0e4a0c1f5dadefa97
Fzekiqi <fazlizekiqi1@hotmail.com>
2019-11-13 14:14:37 +0100

    simple GUI

---
+++ src/client/Client.java
@@ -0,0 +1,55 @@
+package client;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Client extends JFrame {
+
+    JLabel label=new JLabel("Vad heter du?");
+    JButton[] buttons;
+    String[] alternativ={"Fazli","Johan","VictorO","VictorJ"};
+    JPanel panel=new JPanel();
+//    Socket socket=new Socket("localhost",56565);
+//    PrintWriter pw=new PrintWriter(socket.getOutputStream(),true);
+
+    public Client() throws IOException {
+
+
+        buttons=new JButton[4];
+        add(label, BorderLayout.NORTH);
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i]=new JButton(alternativ[i]);
+            buttons[i].addActionListener(clientListener);
+            panel.add(buttons[i], BorderLayout.SOUTH);
+        }
+        add(panel);
+        setSize(400,400);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+
+    ActionListener clientListener=e->{
+        JButton temp=(JButton) e.getSource();
+        System.out.println(temp.getText());
+//        pw.println(temp.getText());
+    };
+
+
+    public static void main(String[] args) {
+        try {
+            new Client();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,6 +1,28 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
+    public static void main(String[] args) {
+        ServerSocket listener;
+        try {
+           listener=new ServerSocket(56565);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+
+        while(true){
+
+            //TODO: new ServerSideGame
+            //TODO: new Player1
+            //TODO: new Player2
+
+
+
+
+        }
+    }
 }
