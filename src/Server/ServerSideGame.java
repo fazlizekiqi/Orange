a90087f9c73c8b2aa964fd34e271036ed69e2803
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-11-14 11:40:35 +0100

    Omarbetade huvudloopen; den bör utföra en hel omgång nu.

--- src/Server/ServerSideGame.java
+++ src/Server/ServerSideGame.java
@@ -8,59 +8,76 @@ public class ServerSideGame {
     ServerSidePlayer currentPlayer;
     private String[] questions; // tills vi har en riktig klass
     private int questionNumber = 0;
-    private int playerOnePoints = 0;
-    private int playerTwoPoints = 0;
+    private int totalRounds;
 
     private static final int SELECTING_CATEGORY = 0;
-    private static final int ASKING_QUESTIONS = 1;
-    private static final int ALL_QUESTIONS_ANSWERED = 2;
+    private static final int ASKING_FIRST_PLAYER = 1;
+    private static final int ASKING_SECOND_PLAYER = 2;
+    private static final int ALL_QUESTIONS_ANSWERED = 3;
     private int currentState = SELECTING_CATEGORY;
 
     // TODO Avgör hur exceptions ska hanteras
     // TODO synchronized?
 
     void gameLoop() throws IOException {
+        for (int currentRound = 1; currentRound <= totalRounds; currentRound++) {
+            playRound();
+        }
+    }
+
+    void playRound() throws IOException {
         // TODO Flytta till en lämplig metod (konstruktor? run? main?)
+        currentState = SELECTING_CATEGORY;
         while (true) {
             if (currentState == SELECTING_CATEGORY) {
-                String category = getCategory();
+                String category = getCategory(currentPlayer);
                 // Hämta rondens frågor
-            } else if (currentState == ASKING_QUESTIONS) {
+                // Något i stil med Database.getQuestions(antal, kategori)
+            } else if (currentState == ASKING_FIRST_PLAYER ||
+            currentState == ASKING_SECOND_PLAYER) {
                 // Notera nuvarande spelaren?
-                String answer = askQuestion();
+                String answer = askQuestion(currentPlayer);
                 if (answer.equals("Det rätta svaret")) {
-                    // Lägg till poäng
+                    currentPlayer.points++;
                 }
-                currentPlayer = currentPlayer.oponentPlayer;
+                nextQuestion();
             } else if (currentState == ALL_QUESTIONS_ANSWERED) {
-                // Avsluta rundan, börja på nästa
+                // Avsluta ronden, börja på nästa
+                sendResults(currentPlayer);
+                sendResults(currentPlayer.oponentPlayer);
+                return; // ?
             }
         }
     }
 
-    String askQuestion() throws IOException {
+    String askQuestion(ServerSidePlayer player) throws IOException {
         currentPlayer.output.writeObject(questions[questionNumber]);
-        return currentPlayer.input.readLine();
+        return player.input.readLine();
     }
 
-    String getCategory() throws IOException {
-        currentPlayer.output.writeObject("Här frågar vi efter en kategori");
-        return currentPlayer.input.readLine();
+    String getCategory(ServerSidePlayer player) throws IOException {
+        player.output.writeObject("Här frågar vi efter en kategori");
+        return player.input.readLine();
     }
 
     void nextQuestion() {
         if (questionNumber < questions.length) {
             questionNumber++;
         } else {
-            currentState = ALL_QUESTIONS_ANSWERED;
+            questionNumber = 0; // Inför nästa rond
+            if (currentState == ASKING_FIRST_PLAYER) {
+                currentState = ASKING_SECOND_PLAYER;
+                currentPlayer = currentPlayer.oponentPlayer;
+            } else {
+                currentState = ALL_QUESTIONS_ANSWERED;
+            }
         }
     }
 
-    void sendResults() throws IOException {
+    void sendResults(ServerSidePlayer player) throws IOException {
         // TODO Formatera resultaten ordentligt
-        Integer[] results = {playerOnePoints, playerTwoPoints};
-        currentPlayer.output.writeObject(results);
-        currentPlayer.oponentPlayer.output.writeObject(results);
+        Integer[] results = {player.points, player.oponentPlayer.points};
+        player.output.writeObject(results);
     }
 
 }
