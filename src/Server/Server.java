6eb226e883b9b3761d76c30e394507cacf65884a
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-11-15 11:18:18 +0100

    Properties

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,9 +1,13 @@
 package Server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.Properties;
 
 public class Server {
+    static int questionsPerRound;
+    static int totalRounds;
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = null;
@@ -13,10 +17,19 @@ public class Server {
             e.printStackTrace();
         }
 
+        Properties p = new Properties();
+        try {
+            p.load(new FileInputStream("properties/testing.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        questionsPerRound = Integer.parseInt(p.getProperty("questions", "2"));
+        totalRounds = Integer.parseInt(p.getProperty("rounds", "2"));
 
         while(true){
 
-            ServerSideGame game=new ServerSideGame(1,1);
+            ServerSideGame game=new ServerSideGame(questionsPerRound, totalRounds);
 
             ServerSidePlayer player1=new ServerSidePlayer(listener.accept(),"Player 1",game);
             ServerSidePlayer player2=new ServerSidePlayer(listener.accept(),"Player 2",game);
