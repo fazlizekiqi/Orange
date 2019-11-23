03c38478c98b4a3f0906fc607db3b615f7cab108
Victor Olofsson <victor.olofsson@yh.nackademin.se>
2019-11-23 13:48:44 +0100

    Effektiviserade Question-klassen

--- src/Database/Category.java
+++ src/Database/Category.java
@@ -4,32 +4,31 @@ import question.Question;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Category {
-    ArrayList<Question> questions = new ArrayList<Question>();
-
-    private String firstLine;
-    private String secondLine;
-    private Question tempQuestion;
+    ArrayList<Question> questions = new ArrayList<>();
 
     Category(String path) {
         try {
             File file = new File(path);
             BufferedReader in = new BufferedReader(new FileReader(file));
+            String firstLine, secondLine;
+            Question tempQuestion;
+
             while ((firstLine = in.readLine()) != null) {
                 tempQuestion = new Question();
                 tempQuestion.setQuestion(firstLine);
                 secondLine = in.readLine();
                 String[] altA = secondLine.split(",");
                 tempQuestion.setRightAnswer(altA[0]);
-                for (int i = 0; i < altA.length; i++) {
-                    tempQuestion.alternatives.add(altA[i]);
-                }
+                tempQuestion.alternatives.addAll(Arrays.asList(altA));
                 Collections.shuffle(tempQuestion.alternatives);
-                Collections.shuffle(questions);
                 questions.add(tempQuestion);
             }
+
+            Collections.shuffle(questions);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
